5b2f81bc04654210b1bb00d8e8c67aa120ac0b01
Philippe Mouchel <philippe.mouchel@savoirfairelinux.com>
2015-04-15 18:04:36 -0400

    Title and new markup in sharer toolbar
    [refs #66075]

--- sharer-toolbar.tpl.php
+++ sharer-toolbar.tpl.php
@@ -6,12 +6,15 @@
  */
 
 ?>
-<ul id="sharer-toolbar">
-  <?php foreach ($sharers as $sid => $sharer) : ?>
-    <li class="shareLink__<?php print $sid; ?>">
-      <a href="<?php print $sharer['url'];?>" title="<?php print t($sharer['label']);?>">
-        <i class="<?php print $sharer['icon_class'];?>"></i><?php print t($sharer['label']);?>
-      </a>
-    </li>
-  <?php endforeach; ?>
-</ul>
+<div id="sharer-toolbar-container">
+  <div id="sharer-toolbar"><?php print $title; ?></div>
+  <ul id="sharer-toolbar">
+    <?php foreach ($sharers as $sid => $sharer) : ?>
+      <li class="shareLink__<?php print $sid; ?>">
+        <a href="<?php print $sharer['url']; ?>" title="<?php print t($sharer['label']); ?>">
+          <i class="<?php print $sharer['icon_class']; ?>"></i><?php print t($sharer['label']); ?>
+        </a>
+      </li>
+    <?php endforeach; ?>
+  </ul>
+</div>
