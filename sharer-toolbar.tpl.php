348dcbf17a2c4cada04d1895410cdb3b2837c7d4
Philippe Mouchel <philippe.mouchel@savoirfairelinux.com>
2016-03-28 17:29:11 -0400

    Fix way to get ordered list to print it out

--- sharer-toolbar.tpl.php
+++ sharer-toolbar.tpl.php
@@ -9,9 +9,9 @@
 <div class="sharer-toolbar-container shareLink">
   <button class="sharer-toolbar-btn shareLink__trigger"><?php print t($title); ?></button>
   <ul class="sharer-toolbar-list shareLink__list">
-    <?php foreach ($sharers as $sid => $sharer) : ?>
-      <li class="shareLink__item shareLink__item--<?php print $sid; ?>">
-        <a class="shareLink__link shareLink__link--<?php print $sid; ?>" href="<?php print $sharer['url']; ?>" data-title="<?php print t($sharer['label']); ?>">
+    <?php foreach ($sharers as $delta => $sharer) : ?>
+      <li class="shareLink__item shareLink__item--<?php print $sharer['sid']; ?>">
+        <a class="shareLink__link shareLink__link--<?php print $sharer['sid']; ?>" href="<?php print $sharer['url']; ?>" data-title="<?php print t($sharer['label']); ?>">
           <i class="<?php print $sharer['icon_class']; ?>"></i><?php print t($sharer['label']); ?>
         </a>
       </li>
